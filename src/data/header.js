8427ce54ca6e5c776455d02f2a598242fda1a895
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 18:24:35 +0200

    Created bagItems data

--- src/data/header.js
+++ src/data/header.js
@@ -13,3 +13,15 @@ export const navItems = [
   { name: 'Watch', linkTo: '/watch' },
   { name: 'Wsparcie', linkTo: '/support' }
 ];
+
+export const bagItems = [
+  { name: 'Torba', linkTo: '/shop/bag', icon: 'fas fa-shopping-bag' },
+  { name: 'Ulubione', linkTo: '/shop/favourites', icon: 'far fa-heart' },
+  { name: 'Zamówienia', linkTo: '/shop/orders', icon: 'fas fa-box-open' },
+  { name: 'Konto', linkTo: '/account', icon: 'fas fa-cog' },
+  {
+    name: 'Zaloguj się',
+    linkTo: '/account/signIn',
+    icon: 'fas fa-user-circle'
+  }
+];
