bf94c6fb09558991f63c81f6ebacb221de5147ab
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 16:47:22 +0200

    Added HeaderContainer to App.js structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import FooterContainer from 'containers/Footer';
+import { FooterContainer, HeaderContainer } from 'containers';
 import { directoryItems, legalLinks, informations } from 'data/footer';
 
 function App() {
   return (
     <Router>
+      <HeaderContainer />
       Hello world!
       <FooterContainer
         directoryItems={directoryItems}
