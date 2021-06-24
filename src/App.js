aa20b098a2af067ee4b78a9b7ffbd003f61b01d8
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 13:32:07 +0200

    Added Footer container to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import FooterContainer from 'containers/Footer';
+import { directoryItems, legalLinks, informations } from 'data/footer';
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <Router>
+      Hello world!
+      <FooterContainer
+        directoryItems={directoryItems}
+        legalLinks={legalLinks}
+        informations={informations}
+      />
+    </Router>
+  );
 }
 
 export default App;
