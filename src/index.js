cd43a1ee85d9e54e92d03d0ee44680ead9438dc4
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 11:15:26 +0200

    Created StyledNormalize.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import StyledNormalize from './StyledNormalize';
 
 ReactDOM.render(
   <React.StrictMode>
+    <StyledNormalize />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
