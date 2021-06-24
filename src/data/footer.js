4f84b63882a8ab6601ac9eb9fc80e2810d981fc7
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 13:31:32 +0200

    Created footer data

---
+++ src/data/footer.js
@@ -0,0 +1,62 @@
+export const directoryItems = [
+  {
+    title: 'Kupuj i poznawaj',
+    links: [
+      { name: 'Mac', linkTo: '/mac' },
+      { name: 'iPad', linkTo: '/iPad' },
+      { name: 'iPhone', linkTo: '/iPhone' },
+      { name: 'Watch', linkTo: '/watch' },
+      { name: 'TV', linkTo: '/tv' },
+      { name: 'Music', linkTo: '/music' },
+      { name: 'AirPods', linkTo: '/airPods' },
+      { name: 'iPod touch', linkTo: '/iPodTouch' },
+      { name: 'Akcesoria', linkTo: '/accesories' }
+    ]
+  },
+  {
+    title: 'Usługi',
+    links: [
+      { name: 'Apple Music', linkTo: '/appleMusic' },
+      { name: 'Apple TV+', linkTo: '/appleTV+' },
+      { name: 'Apple Arcade', linkTo: '/appleArcade' },
+      { name: 'iCloud', linkTo: '/iCloud' },
+      { name: 'Apple One', linkTo: '/appleOne' },
+      { name: 'Apple Pay', linkTo: '/applePay' },
+      { name: 'Apple Store', linkTo: '/appleStore' }
+    ]
+  },
+  {
+    title: 'Apple Store',
+    links: [
+      { name: 'Kupuj online', linkTo: '/buyOnline' },
+      { name: 'Sklep Apple', linkTo: '/appleStore' },
+      { name: 'Stan zamówienia', linkTo: '/orderStatus' },
+      { name: 'Pomoc w zakupach', linkTo: '/shoppingAssistance' }
+    ]
+  },
+  {
+    title: 'O firmie Apple',
+    links: [
+      { name: 'Newsroom', linkTo: '/newsroom' },
+      { name: 'Apple Leadership', linkTo: '/appleLeadership' },
+      { name: 'Praca', linkTo: '/job' },
+      { name: 'Gwarancja', linkTo: '/guarantee' },
+      { name: 'Inwestorzy', linkTo: '/inwestors' },
+      { name: 'Kontakt', linkTo: '/contact' }
+    ]
+  }
+];
+
+export const informations = [
+  { name: 'Kraj pochodzenia: Polska' },
+  { name: 'Copyright © 2021 Pear Inc. Wszelkie prawa zastrzeżone.' }
+];
+
+export const legalLinks = [
+  { name: 'Polityka prywatności', linkTo: '/legal/privacy' },
+  { name: 'Wykorzystanie plików cookie', linkTo: '/legal/privacy/cookies' },
+  { name: 'Warunki korzystania', linkTo: '/legal/internetServices/terms' },
+  { name: 'Sprzeda i zwroty', linkTo: 'shop/browse/open/salesPolicies' },
+  { name: 'Informacje prawne', linkTo: '/legal' },
+  { name: 'Mapa witryny', linkTo: '/sitemap' }
+];
