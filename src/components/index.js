e2eb6350f769ea3a9d987c988aa76df031840103
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 13:31:16 +0200

    Created index.js to export components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import DirectoryComponent from './Directory';
+import FooterComponent from './Footer';
+import WrapperComponent from './Wrapper';
+
+export { DirectoryComponent, FooterComponent, WrapperComponent };
