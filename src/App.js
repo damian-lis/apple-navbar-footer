780eb2f0b1120160a2a78540e79928e6e7567e9d
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 18:33:20 +0200

    Passed bagItems to HeaderContainer in App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { FooterContainer, HeaderContainer } from 'containers';
-import { navItems, navIcons } from 'data/header';
+import { navItems, navIcons, bagItems } from 'data/header';
 import { directoryItems, legalLinks, informations } from 'data/footer';
 
 function App() {
   return (
     <Router>
-      <HeaderContainer navIcons={navIcons} navItems={navItems} />
+      <HeaderContainer navIcons={navIcons} navItems={navItems} bagItems={bagItems} />
       Hello world!
       <FooterContainer
         directoryItems={directoryItems}
