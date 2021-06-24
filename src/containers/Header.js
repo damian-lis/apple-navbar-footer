bc6150d97e589a27a6445cd094d5ec5de9dbfab5
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 18:31:58 +0200

    Added Bag component to Header container

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import { HeaderComponent } from 'components';
+import React, { useContext } from 'react';
+import { HeaderComponent, BagComponent } from 'components';
+import { HeaderContext } from 'contexts';
 
-const HeaderContainer = ({ navItems, navIcons }) => {
-  return <HeaderComponent navItems={navItems} navIcons={navIcons}></HeaderComponent>;
+const HeaderContainer = ({ navItems, navIcons, bagItems }) => {
+  const { bagOpen } = useContext(HeaderContext.store);
+
+  return (
+    <HeaderComponent navItems={navItems} navIcons={navIcons}>
+      {bagOpen && <BagComponent bagItems={bagItems} />}
+    </HeaderComponent>
+  );
 };
 
-export default HeaderContainer;
+const HeaderContainerWrapper = ({ ...restProps }) => {
+  return (
+    <HeaderContext.Provider>
+      <HeaderContainer {...restProps} />
+    </HeaderContext.Provider>
+  );
+};
+
+export default HeaderContainerWrapper;
