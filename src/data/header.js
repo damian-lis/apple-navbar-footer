c2c258ed897ecaba5c6b7d1412b5d3dcdc9ec5ee
Damian Lis <foxprogramming93@gmail.com>
2021-06-24 16:47:36 +0200

    Created header data

---
+++ src/data/header.js
@@ -0,0 +1,15 @@
+export const navIcons = {
+  hamburgerIcon: 'fas fa-bars',
+  logoIcon: 'fas fa-leaf',
+  searchIcon: 'fas fa-search',
+  bagIcon: 'fas fa-shopping-bag',
+  closeIcon: 'fas fa-times'
+};
+
+export const navItems = [
+  { name: 'Mac', linkTo: '/mac' },
+  { name: 'iPad', linkTo: '/ipad' },
+  { name: 'iPhone', linkTo: '/iphone' },
+  { name: 'Watch', linkTo: '/watch' },
+  { name: 'Wsparcie', linkTo: '/support' }
+];
