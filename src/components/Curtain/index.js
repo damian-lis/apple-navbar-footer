6d783e05407270aba2ef71d99ff73f7283c114b9
Damian Lis <foxprogramming93@gmail.com>
2021-06-27 09:56:18 +0200

    Set setCurtain and setSearch fn on Curtain component

--- src/components/Curtain/index.js
+++ src/components/Curtain/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container } from './styles';
+import { CurtainContext, HeaderContext } from 'contexts';
 
 const CurtainComponent = ({ ...restProps }) => {
-  return <Container {...restProps} />;
+  const { setCurtain } = useContext(CurtainContext.store);
+  const { setSearch } = useContext(HeaderContext.store);
+
+  return (
+    <Container
+      onClick={() => {
+        setCurtain(false);
+        setSearch(false);
+      }}
+      {...restProps}
+    />
+  );
 };
 
 export default CurtainComponent;
